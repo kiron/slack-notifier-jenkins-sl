8029b9bb0bde548b1665585fe73f94e848a7fdda
CSNWEB <CSNWEB@users.noreply.github.com>
2018-04-12 01:31:31 +0200

    Get all information by default

--- src/org/gradiant/jenkins/slack/Config.groovy
+++ src/org/gradiant/jenkins/slack/Config.groovy
@@ -3,15 +3,15 @@ package org.gradiant.jenkins.slack
 
 Boolean getNotifySuccess() {
   if(env.NOTIFY_SUCCESS) return new Boolean(env.NOTIFY_SUCCESS)
-  return false
+  return true
 }
 
 Boolean getChangeList() {
   if(env.CHANGE_LIST) return new Boolean(env.CHANGE_LIST)
-  return false
+  return true
 }
 
 Boolean getTestSummary() {
   if(env.TEST_SUMMARY) return new Boolean(env.TEST_SUMMARY)
-  return false
+  return true
 }
